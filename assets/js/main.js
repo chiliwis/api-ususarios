f4c770e016d6e44e8799588f487d0fd6f4de707c
chiliwis <cristinasanchezg@gmail.com>
2022-09-17 13:47:53 -0500

    cargar data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,56 @@
 console.log("Entro al main.js");
 
+const base_url_api = "http://ucamp.alumnos.dev4humans.com.mx";
+const tblUsuarios = document.getElementById("tblUsuarios");
+
+function cargarUsuarios(){
+    fetch(base_url_api + "/Main/alumnos")
+    .then(response => response.json())
+    .then(result=> {
+        console.log(result);
+        tblUsuarios.innerHTML = "";
+        for (const usuario of result.data) {
+            let tr = `<tr>
+            <td>${usuario.id}</td>
+            <td>${usuario.nombre}</td>
+            <td>${usuario.paterno}</td>
+            <td>${usuario.materno}</td>
+            <td>${usuario.nombre}</td>
+            </tr>`;
+            tblUsuarios.innerHTML += tr;
+        }
+})
+    .catch(error => {
+        console.log("Error detectado");
+    })
+}
 function agregarUsuario() {
     console.log("Entro a agregar usuario");
+    let form_data = new FormData();
+   
+    form_data.append("nombre", document.getElementById("nombre").value);
+    form_data.append("paterno", document.getElementById("paterno").value);
+    form_data.append("materno", document.getElementById("materno").value);
+    form_data.append("email", document.getElementById("email").value);
+    fetch(base_url_api + "/Main/alumnos",
+    {
+        method: "POST",
+        body: form_data
+    }
+    )
+    .then(response => response.json())
+    .then(result=> {
+        console.log(result);
+        limpiarFormulario();
+        cargarUsuario();
+    })
+    .catch(error => {
+        console.log("Error detectado");
+    })
 }
+
+function limpiarFormulario(){
+
+}
+
+cargarUsuarios();
